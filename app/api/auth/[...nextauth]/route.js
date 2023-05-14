b55b7e331451a1936d27000f51774a8e7051ee09
Timilehin Arigbede <harriemannie@gmail.com>
2023-05-14 21:21:39 +0100

    Complete Authentication | login, logout and session check

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { signIn } from "next-auth/react";
+import User from "@models/users";
 import { connectToDB } from "@utils/database";
 
 // console.log({
@@ -15,14 +16,27 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({ email: session.user.email });
+
+    sessionUser.id = sessionUser._id.toString();
+    return session;
+  },
   async signIn({ profile }) {
     try {
       await connectToDB();
 
       // check if a user already exists
+      const userExists = await User.findOne({ email: profile.email });
 
       // if not, create a nnew user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
 
       return true;
     } catch (error) {
