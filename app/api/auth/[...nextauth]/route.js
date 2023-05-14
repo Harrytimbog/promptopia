10d6962938345b85d358f173d3e7ab5fffcebbfb
Timilehin Arigbede <harriemannie@gmail.com>
2023-05-14 20:54:47 +0100

    Integrate Nextauth and connect to Database

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { signIn } from "next-auth/react";
+import { connectToDB } from "@utils/database";
+
+// console.log({
+//   clientId: process.env.GOOGLE_ID,
+//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+// });
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+  async session({ session }) {},
+  async signIn({ profile }) {
+    try {
+      await connectToDB();
+
+      // check if a user already exists
+
+      // if not, create a nnew user
+
+      return true;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  },
+});
+
+export { handler as GET, handler as POST };
